3f718efbe2926b0a752c5125f2f97211351a2dab
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-02-17 13:56:35 -0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeSlideComponent } from './home-slide/home-slide.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    HomeSlideComponent
+    HomeSlideComponent,
+    NoticiasComponent,
+    HistoriaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
