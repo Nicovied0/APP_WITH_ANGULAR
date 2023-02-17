78e4359a4e5bd93bd79591bf0ce84192853e62d4
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-02-17 02:31:00 -0300

    commit with first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeSlideComponent } from './home-slide/home-slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeSlideComponent
   ],
   imports: [
     BrowserModule
